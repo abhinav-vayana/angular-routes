f7ff502687d9386a6032e69b3638d21b1ef62588
Abhinav Nair <abhinav.nair@vayana.com>
2025-05-16 15:36:16 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,9 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  standalone: false,
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'router-app';
---
+++ src/app/second/second.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-second',
+  templateUrl: './second.component.html',
+  standalone: false,
+  styleUrl: './second.component.css',
+})
+export class SecondComponent {}
