628920c8984e5c067f425a52670b6cc7e3654add
Abhinav Nair <abhinav.nair@vayana.com>
2025-05-16 16:21:18 +0530

    changed to ng modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FirstComponent } from './first/first.component';
 import { SecondComponent } from './second/second.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
 const routes: Routes = [
-  { path: 'first ', component: FirstComponent },
+  { path: '', component: FirstComponent },
   { path: 'second', component: SecondComponent },
+  { path: '**', component: NotfoundComponent },
 ];
 
 @NgModule({
